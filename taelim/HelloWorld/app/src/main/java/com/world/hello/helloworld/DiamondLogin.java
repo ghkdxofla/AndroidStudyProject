83936cf49fc1256f97f7bc2c8be5c286874aec4c
ghkdxofla <ghkdxofla@gmail.com>
2018-06-28 14:35:27 +0900

    Make login page

---
+++ taelim/HelloWorld/app/src/main/java/com/world/hello/helloworld/DiamondLogin.java
@@ -0,0 +1,23 @@
+package com.world.hello.helloworld;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
+public class DiamondLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_diamond_login);
+
+
+    }
+
+}
